Comfy-Org/ComfyUI_frontend

Add output explorer

src/stores/workspace/sidebarTabStore.ts
@@ -3,6 +3,7 @@ import { computed, ref } from 'vue'
 
 import { useModelLibrarySidebarTab } from '@/hooks/sidebarTabs/modelLibrarySidebarTab'
 import { useNodeLibrarySidebarTab } from '@/hooks/sidebarTabs/nodeLibrarySidebarTab'
+import { useOutputExplorerSidebarTab } from '@/hooks/sidebarTabs/outputExplorerSidebarTab'
 import { useQueueSidebarTab } from '@/hooks/sidebarTabs/queueSidebarTab'
 import { useWorkflowsSidebarTab } from '@/hooks/sidebarTabs/workflowsSidebarTab'
 import { useCommandStore } from '@/stores/commandStore'
@@ -58,6 +59,7 @@ export const useSidebarTabStore = defineStore('sidebarTab', () => {
     registerSidebarTab(useNodeLibrarySidebarTab())
     registerSidebarTab(useModelLibrarySidebarTab())
     registerSidebarTab(useWorkflowsSidebarTab())
+    registerSidebarTab(useOutputExplorerSidebarTab())
   }
 
   return {

hanzoui/frontend

src/stores/workspace/sidebarTabStore.ts
@@ -3,6 +3,7 @@ import { computed, ref } from 'vue'
 
 import { useModelLibrarySidebarTab } from '@/composables/sidebarTabs/useModelLibrarySidebarTab'
 import { useNodeLibrarySidebarTab } from '@/composables/sidebarTabs/useNodeLibrarySidebarTab'
+import { useOutputExplorerSidebarTab } from '@/composables/sidebarTabs/outputExplorerSidebarTab'
 import { useQueueSidebarTab } from '@/composables/sidebarTabs/useQueueSidebarTab'
 import { useWorkflowsSidebarTab } from '@/composables/sidebarTabs/useWorkflowsSidebarTab'
 import { t, te } from '@/i18n'
@@ -92,6 +93,7 @@ export const useSidebarTabStore = defineStore('sidebarTab', () => {
     registerSidebarTab(useNodeLibrarySidebarTab())
     registerSidebarTab(useModelLibrarySidebarTab())
     registerSidebarTab(useWorkflowsSidebarTab())
+<<<<<<< HEAD
 
     const menuStore = useMenuItemStore()
 
@@ -111,6 +113,9 @@ export const useSidebarTabStore = defineStore('sidebarTab', () => {
       ['View'],
       ['Comfy.Canvas.ZoomIn', 'Comfy.Canvas.ZoomOut', 'Comfy.Canvas.FitView']
     )
+=======
+    registerSidebarTab(useOutputExplorerSidebarTab())
+>>>>>>> e9f3e42a (Add output explorer)
   }
 
   return {