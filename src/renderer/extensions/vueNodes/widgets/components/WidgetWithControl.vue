Comfy-Org/ComfyUI_frontend

Wip control widget state migration

src/renderer/extensions/vueNodes/widgets/components/WidgetWithControl.vue
@@ -1,0 +1,58 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+import { computed, defineAsyncComponent, ref } from 'vue'
+import type { Ref } from 'vue'
+
+import type {
+  ControlWidgetOptions,
+  SimplifiedControlWidget
+} from '@/types/simplifiedWidget'
+
+const NumberControlPopover = defineAsyncComponent(
+  () => import('./NumberControlPopover.vue')
+)
+
+function useControlButtonIcon(controlMode: Ref<ControlWidgetOptions>) {
+  return computed(() => {
+    switch (controlMode.value) {
+      case 'increment':
+        return 'pi pi-plus'
+      case 'decrement':
+        return 'pi pi-minus'
+      case 'fixed':
+        return 'icon-[lucide--pencil-off]'
+      case 'global':
+        return 'pi pi-link'
+      default:
+        return 'icon-[lucide--shuffle]'
+    }
+  })
+}
+
+const props = defineProps<{
+  widget: SimplifiedControlWidget<number>
+  comp: unknown
+}>()
+
+const popover = ref()
+
+const controlButtonIcon = useControlButtonIcon(props.widget.controlWidget())
+
+const togglePopover = (event: Event) => {
+  popover.value.toggle(event)
+}
+</script>
+
+<template>
+  <component :is="comp" v-bind="$attrs" :widget>
+    <Button
+      variant="link"
+      size="small"
+      class="h-4 w-7 self-center rounded-xl bg-blue-100/30 p-0"
+      @click="togglePopover"
+    >
+      <i :class="`${controlButtonIcon} text-blue-100 text-xs`" />
+    </Button>
+  </component>
+  <NumberControlPopover ref="popover" :control-widget="widget.controlWidget" />
+</template>

src/types/simplifiedWidget.ts
@@ -2,6 +2,8 @@
  * Simplified widget interface for Vue-based node rendering
  * Removes all DOM manipulation and positioning concerns
  */
+import type { Ref } from 'vue'
+
 import type { InputSpec as InputSpecV2 } from '@/schemas/nodeDef/nodeDefSchemaV2'
 
 /** Valid types for widget values */
@@ -20,10 +22,7 @@ export type ControlWidgetOptions =
   | 'increment'
   | 'decrement'
   | 'randomize'
-export type SafeControlWidget = {
-  value: ControlWidgetOptions
-  update: (value: unknown) => void
-}
+  | 'global'
 
 export interface SimplifiedWidget<
   T extends WidgetValue = WidgetValue,
@@ -56,5 +55,7 @@ export interface SimplifiedWidget<
   /** Optional method to compute widget size requirements */
   computeSize?: () => { minHeight: number; maxHeight?: number }
 
-  controlWidget?: SafeControlWidget
+  controlWidget?: () => Ref<ControlWidgetOptions>
 }
+export type SimplifiedControlWidget<T extends WidgetValue = WidgetValue> =
+  SimplifiedWidget<T> & Required<Pick<SimplifiedWidget<T>, 'controlWidget'>>

hanzoui/frontend

src/renderer/extensions/vueNodes/widgets/components/WidgetWithControl.vue
@@ -1,0 +1,56 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+import { computed, defineAsyncComponent, ref } from 'vue'
+import type { Ref } from 'vue'
+
+import type {
+  ControlOptions,
+  SimplifiedControlWidget
+} from '@/types/simplifiedWidget'
+
+const NumberControlPopover = defineAsyncComponent(
+  () => import('./NumberControlPopover.vue')
+)
+
+function useControlButtonIcon(controlMode: Ref<ControlOptions>) {
+  return computed(() => {
+    switch (controlMode.value) {
+      case 'increment':
+        return 'pi pi-plus'
+      case 'decrement':
+        return 'pi pi-minus'
+      case 'fixed':
+        return 'icon-[lucide--pencil-off]'
+      default:
+        return 'icon-[lucide--shuffle]'
+    }
+  })
+}
+
+const props = defineProps<{
+  widget: SimplifiedControlWidget<number>
+  comp: unknown
+}>()
+
+const popover = ref()
+
+const controlButtonIcon = useControlButtonIcon(props.widget.controlWidget())
+
+const togglePopover = (event: Event) => {
+  popover.value.toggle(event)
+}
+</script>
+
+<template>
+  <component :is="comp" v-bind="$attrs" :widget>
+    <Button
+      variant="link"
+      size="small"
+      class="h-4 w-7 self-center rounded-xl bg-blue-100/30 p-0"
+      @click="togglePopover"
+    >
+      <i :class="`${controlButtonIcon} text-blue-100 text-xs`" />
+    </Button>
+  </component>
+  <NumberControlPopover ref="popover" :control-widget="widget.controlWidget" />
+</template>

src/types/simplifiedWidget.ts
@@ -2,6 +2,8 @@
  * Simplified widget interface for Vue-based node rendering
  * Removes all DOM manipulation and positioning concerns
  */
+import type { Ref } from 'vue'
+
 import type { InputSpec as InputSpecV2 } from '@/schemas/nodeDef/nodeDefSchemaV2'
 
 /** Valid types for widget values */
@@ -30,11 +32,6 @@ function isControlOption(val: WidgetValue): val is ControlOptions {
 export function normalizeControlOption(val: WidgetValue): ControlOptions {
   if (isControlOption(val)) return val
   return 'randomize'
-}
-
-export type SafeControlWidget = {
-  value: ControlOptions
-  update: (value: WidgetValue) => void
 }
 
 export interface SimplifiedWidget<
@@ -70,5 +67,8 @@ export interface SimplifiedWidget<
   /** Optional input specification backing this widget */
   spec?: InputSpecV2
 
-  controlWidget?: SafeControlWidget
+  controlWidget?: () => Ref<ControlOptions>
+
 }
+export type SimplifiedControlWidget<T extends WidgetValue = WidgetValue> =
+  SimplifiedWidget<T> & Required<Pick<SimplifiedWidget<T>, 'controlWidget'>>