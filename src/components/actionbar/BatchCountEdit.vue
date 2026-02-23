f684f11db7058b935ab160963fb407523ac4a991
Benjamin Lu <benjaminlu1107@gmail.com>
2026-02-23 06:31:23 -0800

    fix: align run controls styling with queue modal design

--- src/components/actionbar/BatchCountEdit.vue
+++ src/components/actionbar/BatchCountEdit.vue
@@ -9,26 +9,78 @@
   >
     <InputNumber
       v-model="batchCount"
-      class="w-14"
+      class="h-full w-14"
       :min="minQueueCount"
       :max="maxQueueCount"
       fluid
       show-buttons
       :pt="{
+        root: {
+          class: 'h-full rounded-l-lg bg-secondary-background'
+        },
+        pcInputText: {
+          root: {
+            class:
+              'h-full border-secondary-background bg-secondary-background text-sm font-normal tabular-nums text-base-foreground'
+          }
+        },
+        buttonGroup: {
+          class: 'h-full bg-secondary-background'
+        },
         incrementButton: {
-          class: 'w-6 rounded-tr-none',
+          class:
+            'h-full w-6 rounded-tr-none bg-secondary-background border-secondary-background text-base-foreground hover:bg-secondary-background hover:border-secondary-background',
           onmousedown: () => {
             handleClick(true)
           }
         },
         decrementButton: {
-          class: 'w-6 rounded-br-none',
+          class:
+            'h-full w-6 rounded-br-none bg-secondary-background border-secondary-background text-base-foreground hover:bg-secondary-background hover:border-secondary-background',
           onmousedown: () => {
             handleClick(false)
           }
         }
       }"
-    />
+    >
+      <template #incrementicon>
+        <svg
+          class="rotate-180"
+          xmlns="http://www.w3.org/2000/svg"
+          width="8"
+          height="5"
+          viewBox="0 0 8 5"
+          fill="none"
+          aria-hidden="true"
+        >
+          <path
+            d="M0.650391 0.649902L3.65039 3.6499L6.65039 0.649902"
+            stroke="#8A8A8A"
+            stroke-width="1.3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </template>
+      <template #decrementicon>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="8"
+          height="5"
+          viewBox="0 0 8 5"
+          fill="none"
+          aria-hidden="true"
+        >
+          <path
+            d="M0.650391 0.649902L3.65039 3.6499L6.65039 0.649902"
+            stroke="#8A8A8A"
+            stroke-width="1.3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </template>
+    </InputNumber>
   </div>
 </template>
 
