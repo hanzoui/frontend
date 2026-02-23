acda72101060aaa8779e81208a74303baa7ad40d
Benjamin Lu <benjaminlu1107@gmail.com>
2026-02-23 06:39:49 -0800

    fix: deduplicate actionbar chevron svg

--- src/components/actionbar/BatchCountEdit.vue
+++ src/components/actionbar/BatchCountEdit.vue
@@ -44,41 +44,10 @@
       }"
     >
       <template #incrementicon>
-        <svg
-          class="rotate-180"
-          xmlns="http://www.w3.org/2000/svg"
-          width="8"
-          height="5"
-          viewBox="0 0 8 5"
-          fill="none"
-          aria-hidden="true"
-        >
-          <path
-            d="M0.650391 0.649902L3.65039 3.6499L6.65039 0.649902"
-            stroke="#8A8A8A"
-            stroke-width="1.3"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
+        <TinyChevronIcon rotate-up />
       </template>
       <template #decrementicon>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="8"
-          height="5"
-          viewBox="0 0 8 5"
-          fill="none"
-          aria-hidden="true"
-        >
-          <path
-            d="M0.650391 0.649902L3.65039 3.6499L6.65039 0.649902"
-            stroke="#8A8A8A"
-            stroke-width="1.3"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
+        <TinyChevronIcon />
       </template>
     </InputNumber>
   </div>
@@ -91,6 +60,7 @@ import { computed } from 'vue'
 
 import { useSettingStore } from '@/platform/settings/settingStore'
 import { useQueueSettingsStore } from '@/stores/queueStore'
+import TinyChevronIcon from './TinyChevronIcon.vue'
 
 const queueSettingsStore = useQueueSettingsStore()
 const { batchCount } = storeToRefs(queueSettingsStore)
